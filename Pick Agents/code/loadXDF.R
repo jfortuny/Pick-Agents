3ea920733d6cda7aec3bcd73375ecf1e059257c9
jfortuny <jfortuny@jfortuny.com>
2016-11-07 16:09:56 -0500

    data cleansing

--- Pick Agents/code/loadXDF.R
+++ Pick Agents/code/loadXDF.R
@@ -7,3 +7,9 @@ summary(dsDiscovery)
 
 head(dsDiscovery)
 
+dsDiscovery[dsDiscovery$CRMGender == "U",]$CRMGender <- NA
+dsDiscovery[dsDiscovery$CRMCounty == "--",]$CRMCounty <- NA
+dsDiscovery[dsDiscovery$CRMZipCode == "-----",]$CRMZipCode <- NA
+
+table(dsDiscovery$CRMCounty)
+summary(dsDiscovery$CRMZipCode)
